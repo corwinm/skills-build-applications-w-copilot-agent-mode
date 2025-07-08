f89fb4c7fc2deeae3756ccd127f8907c063b139e
Corwin Marsh <corwinm@users.noreply.github.com>
2025-07-08 14:03:44 -0700

    Added codespace URL to ALLOWED_HOSTS in settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lq1k!e%t38ak)e9oqz4*dpd75%@b$0do+ab0m=u^lx-(^yg)mj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cautious-funicular-r5pqpx9746fpj7x-8000.app.github.dev']
 
 
 # Application definition
